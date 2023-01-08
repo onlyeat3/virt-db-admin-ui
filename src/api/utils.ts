80bdcf1ef536ae6211b5355bfd358c58ead3fc67
onlyeat3 <onlyeat3@gmail.com>
2023-01-08 15:09:17 +0800

    feat: 去掉mock

---
+++ src/api/utils.ts
@@ -0,0 +1,5 @@
+export const baseUrlApi = `http://127.0.0.1:8080`;
+// export const baseUrlApi = (url: string) =>
+//   process.env.NODE_ENV === "development"
+//     ? `/api/${url}`
+//     : `http://127.0.0.1:3000/${url}`;
--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -13,11 +13,13 @@ import { stringify } from "qs";
 import NProgress from "../progress";
 import { getToken, formatToken } from "@/utils/auth";
 import { useUserStoreHook } from "@/store/modules/user";
+import { baseUrlApi } from "@/api/utils";
 
 // 相关配置请参考：www.axios-js.com/zh-cn/docs/#axios-request-config-1
 const defaultConfig: AxiosRequestConfig = {
   // 请求超时时间
   timeout: 10000,
+  baseURL: baseUrlApi,
   headers: {
     Accept: "application/json, text/plain, */*",
     "Content-Type": "application/json",
